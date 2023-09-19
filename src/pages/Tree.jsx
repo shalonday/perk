3b53530c3b124b01985b4a70871420a355e9fd87
shalonday <69411982+shalonday@users.noreply.github.com>
2023-09-19 18:50:17 +0800

    Add purpose comment to <Tree>

--- src/pages/Tree.jsx
+++ src/pages/Tree.jsx
@@ -1,3 +1,6 @@
+// Produces the main viewing page for a tree--a traversable visual representation of a tree that opens
+// a node- or path-specific modal that shows their details upon click.
+
 function Tree() {
   return <div>Tree</div>;
 }
