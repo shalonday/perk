0bf8402590ef838bba2745dc0e69f6c29ef471ed
shalonday <69411982+shalonday@users.noreply.github.com>
2023-09-19 17:48:38 +0800

    Add majority of Search.jsx contents
    I am copying most of this code from a previous one that I already made,
    but which did not use git so please bear with me if I commit a lot of
    these initial ones in large batches.
    
    This adds

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,5 +1,30 @@
+import { useState } from "react";
+
 function Search() {
-  return <div>Search</div>;
+  const [query, setQuery] = useState("");
+  const createdTrees = [
+    {
+      id: "2ef94cdd-4077-4c36-92bb-5a3b09fc44d1",
+      title: "ZTM's Become a Freelance Developer",
+      desc: "The 'Become a Freelance Developer' Career Path from Zero To Mastery. Accessible via https://zerotomastery.io/career-paths/become-a-freelancer-2r7slf",
+      tree: { skillIds: [0, 1, 2, 3, 4], lpathIds: [1, 2, 3, 4] },
+    },
+  ]; //sample
+  return (
+    <div>
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      {createdTrees.map((tree) => (
+        <div key={tree.id}>
+          <h3>{tree.title}</h3>
+          <p>{tree.desc}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Search;
