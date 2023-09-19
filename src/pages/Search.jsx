a0812b8875079dcdef89e45d17fd9ee53af97665
shalonday <69411982+shalonday@users.noreply.github.com>
2023-09-19 17:52:15 +0800

    Add search input to Search.jsx
    Please take note that I will be copying a lot of this code from previously
    written code but which did not use git. However I am trying to commit
    this one piece of code at a time in a way that makes sense.
    
    This adds the search input element with accompanying useState. Also
    added a placeholder for where the createdTrees will appear, and a
    button that will later link to a tree creation screen.

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,14 +2,7 @@ import { useState } from "react";
 
 function Search() {
   const [query, setQuery] = useState("");
-  const createdTrees = [
-    {
-      id: "2ef94cdd-4077-4c36-92bb-5a3b09fc44d1",
-      title: "ZTM's Become a Freelance Developer",
-      desc: "The 'Become a Freelance Developer' Career Path from Zero To Mastery. Accessible via https://zerotomastery.io/career-paths/become-a-freelancer-2r7slf",
-      tree: { skillIds: [0, 1, 2, 3, 4], lpathIds: [1, 2, 3, 4] },
-    },
-  ]; //sample
+
   return (
     <div>
       <input
@@ -17,12 +10,8 @@ function Search() {
         value={query}
         onChange={(e) => setQuery(e.target.value)}
       />
-      {createdTrees.map((tree) => (
-        <div key={tree.id}>
-          <h3>{tree.title}</h3>
-          <p>{tree.desc}</p>
-        </div>
-      ))}
+      {/* put created trees here */}
+      <button>Create Tree</button>
     </div>
   );
 }
