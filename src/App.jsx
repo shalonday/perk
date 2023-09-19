659c0790eb0d964c8403349dabc35c424d76b935
shalonday <69411982+shalonday@users.noreply.github.com>
2023-09-19 17:27:41 +0800

    Add routes and pages, configure eslint
    eslint didn't come automatically with Vite this time around so I had
    to follow Jonas' (React Udemy course) eslint config instructions.
    
    There was also one rogue import that I wasn't able to remove the previous
    commit.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Search from "./pages/Search";
+import Tree from "./pages/Tree";
+import PageNotFound from "./pages/PageNotFound";
+import Edit from "./pages/Edit";
 
 function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Search />} />
+        <Route path="tree" element={<Tree />} />
+        <Route path="edit" element={<Edit />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
