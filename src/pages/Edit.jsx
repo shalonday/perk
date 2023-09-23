dd1a939e746bbe6d4777588e228062e3811422a1
shalonday <69411982+shalonday@users.noreply.github.com>
2023-09-23 19:56:49 +0800

    Map nodesArray to produce sphere per node
    Needed to see if Babylon spheres would be amenable to being produced
    using array methods

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -10,52 +10,6 @@ import {
 import SceneComponent from "../components/SceneComponent";
 import "./Edit.css";
 
-/////////////////////////////////////////////////////////////////
-///////////REQUIRED FOR INSERTING BABYLONJS INTO REACT///////////
-/////////////////////////////////////////////////////////////////
-let box;
-
-function onSceneReady(scene) {
-  // This creates and positions a free camera (non-mesh)
-  const camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
-
-  //This targets the camera to scene origin
-  camera.setTarget(Vector3.Zero());
-
-  const canvas = scene.getEngine().getRenderingCanvas();
-
-  //This attaches the camera to the canvas
-  camera.attachControl(canvas, true);
-
-  //This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-  const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
-
-  //Default intensity is 1. Let's dim the light a small amount
-  light.intensity = 0.7;
-
-  //Our built-in 'box' shape
-  box = MeshBuilder.CreateBox("box", { size: 2 }, scene);
-
-  //Move the box upward 1/2 its height
-  box.position.y = 1;
-
-  // Our built-in 'ground' shape
-  MeshBuilder.CreateGround("ground", { width: 6, height: 6 }, scene);
-}
-
-// Will run on every frame render. We are spinning the box on y-axis.
-
-function onRender(scene) {
-  if (box !== undefined) {
-    const deltaTimeInMillis = scene.getEngine().getDeltaTime();
-    const rpm = 10;
-    box.rotation.y += (rpm / 60) * Math.PI * 2 * (deltaTimeInMillis / 1000);
-  }
-}
-/////////////////////////////////////////////////////////
-//////////////END OF BABYLON SECTION/////////////////////
-/////////////////////////////////////////////////////////
-
 const tempNodesList = [
   { id: 0, title: "Node 1", type: "node", detailsArray: ["Prerequisite node"] },
   {
@@ -99,20 +53,53 @@ function Edit() {
   const [pathsArray, setPathsArray] = useState(tempPathsList);
 
   /*
-    nodesArray is Array[{Node}]
-    interp. array of node objects
-  
-    Node is Object{
-      id is Int
-      title is String
-      type is one of ["node", "path"] 
-        - interp. always "node" for node objects. Allows Modal component to distinguish between paths and nodes.
-      detailsArray is Array[String]
-        - the strings that compose the description bullets. In the case of nodes, these are SKILLS.
-    }
-    example: { id: 1, title: "Node 1", type: "node", detailsArray: ["Prerequisite node"] }
-     */
+      nodesArray is Array[{Node}]
+      interp. array of node objects
+    
+      Node is Object{
+        id is Int
+        title is String
+        type is one of ["node", "path"] 
+          - interp. always "node" for node objects. Allows Modal component to distinguish between paths and nodes.
+        detailsArray is Array[String]
+          - the strings that compose the description bullets. In the case of nodes, these are SKILLS.
+      }
+      example: { id: 1, title: "Node 1", type: "node", detailsArray: ["Prerequisite node"] }
+       */
   const [nodesArray, setNodesArray] = useState(tempNodesList);
+
+  /////////////////////////////////////////////////////////////////
+  ///////////REQUIRED FOR INSERTING BABYLONJS INTO REACT///////////
+  /////////////////////////////////////////////////////////////////
+
+  function onSceneReady(scene) {
+    // This creates and positions a free camera (non-mesh)
+    const camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
+
+    //This targets the camera to scene origin
+    camera.setTarget(Vector3.Zero());
+
+    const canvas = scene.getEngine().getRenderingCanvas();
+
+    //This attaches the camera to the canvas
+    camera.attachControl(canvas, true);
+
+    //This creates a light, aiming 0,1,0 - to the sky (non-mesh)
+    const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
+
+    //Default intensity is 1. Let's dim the light a small amount
+    light.intensity = 0.7;
+
+    const sphereArray = nodesArray.map((node) =>
+      MeshBuilder.CreateSphere("sphere", {}, scene)
+    );
+
+    sphereArray.map((sphere, i) => (sphere.position.x = i + 1));
+  }
+
+  /////////////////////////////////////////////////////////
+  //////////////END OF BABYLON SECTION/////////////////////
+  /////////////////////////////////////////////////////////
 
   return (
     <div className="edit-container">
@@ -123,7 +110,6 @@ function Edit() {
       <SceneComponent
         antialias
         onSceneReady={onSceneReady}
-        onRender={onRender}
         className="edit-visualization"
       />
     </div>
