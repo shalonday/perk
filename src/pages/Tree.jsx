3385e5ac3d40425e32caf0950ca3ca49d7b22e4c
shalonday <69411982+shalonday@users.noreply.github.com>
2023-09-23 16:48:01 +0800

    Add babylon viewer and sample babylon file
    An attempt to see if babylon viewer would work for the site. However
    it was taking too long to load so perhaps I'll use the direct to code
    method that doesn't use Babylon Viewer. I'll keep these files here just
    in case I'll need them in the future though.

--- src/pages/Tree.jsx
+++ src/pages/Tree.jsx
@@ -1,8 +1,12 @@
 // Produces the main viewing page for a tree--a traversable visual representation of a tree that opens
 // a node- or path-specific modal that shows their details upon click.
-
+import "babylonjs-viewer";
 function Tree() {
-  return <div>Tree</div>;
+  return (
+    <div style={{ height: 1000, width: 1000 }}>
+      <babylon model="https://models.babylonjs.com/boombox.glb"></babylon>
+    </div>
+  );
 }
 
 export default Tree;
