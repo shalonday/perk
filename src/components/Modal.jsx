f62d3abd9481a8bd4f26cc63f58c8013c5058095
shalonday <69411982+shalonday@users.noreply.github.com>
2023-09-25 20:49:25 +0800

    Copy Modal component from previous version
    Copy-pasted for convenience. This will probably need a lot of editing.

---
+++ src/components/Modal.jsx
@@ -0,0 +1,89 @@
+import { useState } from "react";
+
+export default function Modal({ source, setSourceArray }) {
+  const [fromNode, setFromNode] = useState(source.fromNode);
+  const [toNode, setToNode] = useState(source.toNode);
+  const [title, setTitle] = useState(source.title);
+  const [bullets, setBullets] = useState(source.detailsArray);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    // use setArray methods here !!!
+
+    source.type === "path" &&
+      setSourceArray((array) =>
+        array.map((item) =>
+          item.id === source.id
+            ? {
+                ...item,
+                title: title,
+                detailsArray: bullets,
+                fromNode: fromNode,
+                toNode: toNode,
+              }
+            : item
+        )
+      );
+
+    //{source.type==="node" && setSourceArray((array)=>)}
+  }
+
+  function handleDeleteItem(index) {
+    setBullets((array) => array.filter((item, i) => i !== index));
+  }
+
+  /*
+  Add an empty item to detailsArray to be edited by user
+  */
+  function handleAddDetail() {
+    setBullets((array) => [...array, ""]);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      {source.type === "path" && (
+        <p>
+          This path connects{" "}
+          <select>
+            <option>{source.fromNode}</option>
+          </select>{" "}
+          and{" "}
+          <select>
+            <option>{source.toNode}</option>
+          </select>
+        </p>
+      )}
+      <h3>
+        <input
+          type="text"
+          placeholder={"Title: " + source.title}
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </h3>
+
+      <ul>
+        {bullets.map((bullet, index) => (
+          <li key={index}>
+            <input
+              type="text"
+              placeholder={bullet}
+              value={bullet}
+              onChange={(e) => {
+                setBullets((array) =>
+                  array.map((item, i) => (i === index ? e.target.value : item))
+                );
+              }}
+            />
+            <span onClick={() => handleDeleteItem(index)}>❌</span>
+          </li>
+        ))}
+        <p onClick={handleAddDetail} key="last">
+          +
+        </p>
+      </ul>
+      <button>Save</button>
+    </form>
+  );
+}
