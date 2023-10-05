74ce23532887b5394d48a3f5977d067aae31f328
shalonday <69411982+shalonday@users.noreply.github.com>
2023-10-05 16:42:55 +0800

    Remove babylon element and viewer
    These are no longer necessary and felt like they were taking up so much
    memory.

--- src/pages/Tree.jsx
+++ src/pages/Tree.jsx
@@ -1,12 +1,7 @@
 // Produces the main viewing page for a tree--a traversable visual representation of a tree that opens
 // a node- or path-specific modal that shows their details upon click.
-import "babylonjs-viewer";
 function Tree() {
-  return (
-    <div style={{ height: 1000, width: 1000 }}>
-      <babylon model="https://models.babylonjs.com/boombox.glb"></babylon>
-    </div>
-  );
+  return <div style={{ height: 1000, width: 1000 }}></div>;
 }
 
 export default Tree;
