48d21df1ed1b0312c71b264397b21b52e6b5ad76
shalonday <69411982+shalonday@users.noreply.github.com>
2023-10-14 10:37:41 +0800

    Add inputs and new css to NewTreeModal
    Decided to give it its own css so I can style the text inputs that are
    not present in the other modal.

--- src/components/NewTreeModal.jsx
+++ src/components/NewTreeModal.jsx
@@ -1,19 +1,42 @@
-import styles from "./Modal.module.css";
+import { useState } from "react";
+import styles from "./NewTreeModal.module.css";
 
 function NewTreeModal({ nodesArray, setIsNewTreeModalVisible }) {
+  const [title, setTitle] = useState("Tree Title");
+  const [description, setDescription] = useState("Tree description here");
   return (
     <>
       <div className={styles.backgroundBox}></div>
       <form className={styles.modal}>
-        Pick starting node(s) for your tree:
-        <select>
-          {nodesArray.map((node) => (
-            <option>{node.title}</option>
-          ))}
-        </select>
-        ...Or{" "}
-        <button onClick={() => setIsNewTreeModalVisible(false)}>create</button>{" "}
-        your own
+        <div className={styles.div}>
+          <input
+            className={styles.input}
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <textarea
+            rows={10}
+            className={styles.textarea}
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          ></textarea>
+        </div>
+        <div className={styles.pickDiv}>
+          <p classname={styles.pickP}>
+            Pick starting node(s) for your tree:
+            <select>
+              {nodesArray.map((node) => (
+                <option>{node.title}</option>
+              ))}
+            </select>
+            ...Or{" "}
+            <button onClick={() => setIsNewTreeModalVisible(false)}>
+              create
+            </button>{" "}
+            your own
+          </p>
+        </div>
       </form>
     </>
   );
