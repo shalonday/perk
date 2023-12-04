4f9fb50232290ba75171e8419d7fe5e82d8681a0
shalonday <69411982+shalonday@users.noreply.github.com>
2023-12-04 15:21:19 +0800

    Changes for deployment
    Deploying to github pages for a free option. I tried deploying through
    my repository's Settings page, but nothing would appear (not even an
    error message on screen; it just says Error 404 on the console) even
    after giving it a few hours. So I tried searching and followed the first
    comment (by "Arnas") on this post:
    https://dev.to/shashannkbawa/deploying-vite-app-to-github-pages-3ane

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import eslint from "vite-plugin-eslint";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
+  base: "/perk/",
   test: {
     includeSource: ["src/**/*.{js,jsx}"],
   },
